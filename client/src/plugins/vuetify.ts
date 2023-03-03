53aba7fc851920eb039c4e203e2526db0132f051
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-03-03 16:55:38 +0700

    :money_with_wings: set color theme

--- client/src/plugins/vuetify.ts
+++ client/src/plugins/vuetify.ts
@@ -1,16 +1,45 @@
-import { createVuetify } from 'vuetify'
-import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/styles'
+import { createVuetify, ThemeDefinition } from "vuetify";
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
+
+const LightTheme: ThemeDefinition = {
+  dark: false,
+  colors: {
+    background: "#FFFFFF",
+    surface: "#FFFFFF",
+    primary: "#6200EE",
+    "primary-darken-1": "#3700B3",
+    secondary: "#03DAC6",
+    "secondary-darken-1": "#018786",
+    error: "#B00020",
+    info: "#2196F3",
+    success: "#4CAF50",
+    warning: "#FB8C00",
+  },
+};
+
+const DarkTheme: ThemeDefinition = {
+  dark: true,
+  colors: {
+    background: "#121212",
+    surface: "#121212",
+    primary: "#6200EE",
+    "primary-darken-1": "#3700B3",
+    secondary: "#03DAC6",
+    "secondary-darken-1": "#018786",
+    error: "#B00020",
+    info: "#2196F3",
+    success: "#4CAF50",
+    warning: "#FB8C00",
+  },
+};
 
 export default createVuetify({
   theme: {
+    defaultTheme: "DarkTheme",
     themes: {
-      light: {
-        colors: {
-          primary: '#1867C0',
-          secondary: '#5CBBF6',
-        },
-      },
+      light: LightTheme,
+      dark: DarkTheme,
     },
   },
-})
+});
