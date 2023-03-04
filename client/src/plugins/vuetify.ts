22ea3052dd8e8052983a987e772ec6e287fb1dbd
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-03-04 17:05:55 +0700

    :money_with_wings: refactor theme

--- client/src/plugins/vuetify.ts
+++ client/src/plugins/vuetify.ts
@@ -1,45 +1,40 @@
-import { createVuetify, ThemeDefinition } from "vuetify";
-import "@mdi/font/css/materialdesignicons.css";
-import "vuetify/styles";
+import { createVuetify, ThemeDefinition } from 'vuetify'
 
-const LightTheme: ThemeDefinition = {
+const lightTheme: ThemeDefinition = {
   dark: false,
   colors: {
-    background: "#FFFFFF",
-    surface: "#FFFFFF",
-    primary: "#6200EE",
-    "primary-darken-1": "#3700B3",
-    secondary: "#03DAC6",
-    "secondary-darken-1": "#018786",
-    error: "#B00020",
-    info: "#2196F3",
-    success: "#4CAF50",
-    warning: "#FB8C00",
+    background: '#FFFFFF',
+    surface: '#FFFFFF',
+    primary: '#6200EE',
+    'primary-darken-1': '#3700B3',
+    secondary: '#03DAC6',
+    'secondary-darken-1': '#018786',
+    error: '#B00020',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FB8C00',
   },
-};
+}
 
-const DarkTheme: ThemeDefinition = {
+const darkTheme: ThemeDefinition = {
   dark: true,
   colors: {
-    background: "#121212",
-    surface: "#121212",
-    primary: "#6200EE",
-    "primary-darken-1": "#3700B3",
-    secondary: "#03DAC6",
-    "secondary-darken-1": "#018786",
-    error: "#B00020",
-    info: "#2196F3",
-    success: "#4CAF50",
-    warning: "#FB8C00",
+    background: '#121212',
+    surface: '#121212',
+    primary: '#6200EE',
+    'primary-darken-1': '#3700B3',
+    secondary: '#03DAC6',
+    'secondary-darken-1': '#018786',
+    error: '#B00020',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FB8C00',
   },
-};
+}
 
 export default createVuetify({
   theme: {
-    defaultTheme: "DarkTheme",
-    themes: {
-      light: LightTheme,
-      dark: DarkTheme,
-    },
+    defaultTheme: 'dark',
+    themes: { light: lightTheme, dark: darkTheme },
   },
-});
+})
